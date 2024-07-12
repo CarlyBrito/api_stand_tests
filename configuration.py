5d3fb2c0e05fe894b875961c60547589eeb4bf92
Carly Brito <bcarlianis@gmail.com>
2024-07-11 20:56:33 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-217aa7ae-8ba5-45da-93e5-9469feac1d92.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Carly",
+    "phone": "+593983989862",
+    "address": "ecuador"
+}
+product_ids = {
+    "ids": [1, 2, 3]
+}
+user_body = {
+    "firstName": "Carli",
+    "phone": "+593983989862",
+    "address": "Ecuador"
+}
